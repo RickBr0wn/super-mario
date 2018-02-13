0d6cd7ca8e8f08dfd710b1ad1b79f264125c5a2c
RiCK BROWN <ricky.brown.00@gmail.com>
2018-02-13 11:47:54 +0000

    Added DEFINE method to SPRITESHEET class

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -13,6 +13,23 @@ class SpriteSheet{
         this.image = image;
         this.width = width;
         this.height = height;
+        this.tiles = new Map();
+    }
+
+    define(name, x, y){
+        const buffer = document.createElement('canvas');
+        buffer.width = this.width;
+        buffer.height = this.height;
+        buffer.getContext('2d').drawImage(this.image,
+            x*this.width, 
+            y*this.height,
+            this.width,
+            this.height,
+            0,
+            0,
+            this.width,
+            this.height);
+        this.tiles.set(name, buffer);
     }
 }
 
